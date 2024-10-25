36b164c495141c49350590ad0479dbdaeef74fd7
Maxi <makushi2121@gmail.com>
2024-10-25 16:58:41 +0200

    prueba2

---
+++ CorredInsensatos.java
@@ -0,0 +1,7 @@
+public class CorredInsensatos {
+	public static void main (String[] args) {
+		System.out.println("Corred! Corred! Corred!");
+		System.out.println("JAAAAA!");
+		System.out.println("Insensatos!");	
+	}
+}
