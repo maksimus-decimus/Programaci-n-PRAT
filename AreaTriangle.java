11db87db6fc172ecea50e16453c0ff44c66cbab3
Maksim Fernández <makushi2121@gmail.com>
2024-10-25 16:30:44 +0200

    prueba

---
+++ AreaTriangle.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class AreaTriangle {
+  public static void main (String args[]){
+    Scanner lector = new Scanner(System.in);
+   
+    System.out.println("Vamos a calcular el área de un triangulo");
+    System.out.println("Escribe la anchura del triangulo:");
+      int b= lector.nextInt();
+
+    System.out.println("Escribe la altura del triangulo:");
+      int h= lector.nextInt(); 
+
+    System.out.println("El área del triangulo es: " + (b*h)/2); 
+
+    //Calculem l'area del triangle: Area = (width*height)/2
+    //Imprimim a la terminal el calcul
+  }
+}
