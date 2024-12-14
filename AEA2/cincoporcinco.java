3ed74279455127e7f64229030370859913c2c37f
maksimus-decimus <makushi2121@gmail.com>
2024-12-14 06:28:08 +0100

    1

--- AEA2/cincoporcinco.java
+++ AEA2/cincoporcinco.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Random;
 
 public class cincoporcinco {
@@ -9,6 +10,7 @@ public class cincoporcinco {
     int pares = 0;
     int numMax = matriz[0][0];
     int contador = 0;
+    ArrayList<Integer> primos = new ArrayList<>(); // He preguntat si es podia fer un nou array per ficar els senars dins, y he intentat aplicar-ho
 
     System.out.println("=================");
     for (int i = 0; i < matriz.length; i++) {
@@ -16,14 +18,7 @@ public class cincoporcinco {
             
             matriz[i][j] = randomNum.nextInt(101);
             total += matriz[i][j];
-            pares += matriz[i][j];
-
-
             
-            System.out.print(matriz[i][j]+ " ");
-             if (pares % 2 == 0) {
-                
-            }
             
              if (matriz[i][j] > numMax) {
                 numMax = matriz[i][j];
@@ -35,16 +30,46 @@ public class cincoporcinco {
 
             }
 
+            if (matriz[i][j] < 2){
+                primos.add(matriz[i][j]);
+                }
+            for (int x = 2; x <= matriz[i][j]; x++){
+                if (matriz[i][j] == x){
+                    primos.add(x);
+                } else if ((matriz[i][j] % x ) == 0){
+                    x = matriz[i][j] + 1;
+                }
+            }
         
-            
+            System.out.print(matriz[i][j]+ " ");    
         }
+
+        System.out.println( " ");
         
-    } 
+    }
+    
     System.out.println("=================");
-    System.out.println("El promedio de los valores de la matriz es: " + pares);
-    System.out.println("El promedio de los valores de la matriz es: " + total/25); 
+    System.out.println("El promedio de los valores de la matriz es: " + total/25);
     System.out.println("El valor maximo de la matriz es: " + numMax);
     System.out.println("El numero de veces que se repite es: " + contador);
+    System.out.println("Números primos:"+primos); 
+    System.out.println("Números pares: ");
+
+    for (int i = 0; i < matriz.length; i++) {
+        for (int j = 0; j < matriz[i].length; j++) {
+            if (pares % 2 == 0) {
+                System.out.print(matriz[i][j] + " ");
+            } else {
+                System.out.println("Números primos: ");
+                System.out.println();
+            }
+
+        }
+    } 
+
+    
+    
+    
 
 
     }             
