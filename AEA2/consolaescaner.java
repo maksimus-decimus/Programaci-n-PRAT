20175edb1ed726370eb9c495da60e0bf69586096
Maxi Fernandez <makushi2121@gmail.com>
2024-12-16 20:29:43 +0100

    xingjian

---
+++ AEA2/consolaescaner.java
@@ -0,0 +1,38 @@
+import  java.util.Scanner;
+
+public class consolaescaner {
+
+    public static void main(String[] args) { 
+        Scanner sc = new Scanner(System.in);
+        boolean activo = true;
+        while (activo) {
+            System.out.println("¿Cuántas repeticiones?");
+            int rep = sc.nextInt();
+            System.out.println("¿Qué caracter desea imprimir?");
+            char caracter = sc.next().charAt(0);
+            funcio(rep, caracter);  
+            System.out.println("¿Desea continuar?");
+            String respuesta = sc.next();
+            if (respuesta.equalsIgnoreCase("Si")) {
+                System.out.println();
+            }
+            else {
+                activo = false;
+            }
+        }
+        
+    }
+
+
+    //UNA ÚNICA FUNCIÓN
+    public static void funcio(int rep, char caracter) {
+        for (int i = 0; i < rep; i++) {
+            System.out.print(caracter);    
+        }
+        System.out.println(" ");
+    }
+
+
+
+}
+
--- AEA2/consolaprueba.java
+++ AEA2/consolaprueba.java
@@ -1,21 +1,55 @@
-public class consola {
+public class consolaprueba {
 
     public static void main(String[] args) { 
+        estrellitas(8);
+        guiones(1);
+        numeros(6, 5);
+        estrellitas(3);
+        guiones(5);
+        numeros(2, 3);
+        System.out.println();
+        System.out.println("=================================");
+        System.out.println("Solo una función:");
+        System.out.println();
         funcio(8, '*');
         funcio(1,'-');
         funcio(6, '5');
         funcio(3, '*');
-        funcio(5, '-');
+        funcio(5, '-');            
         funcio(2, '3');
     }
 
+    //FUNCIONES INDIVIDUALES
+    public static void estrellitas(int rep) {
+        for (int i = 0; i < rep; i++) {
+            System.out.print('*');    
+        }
+        System.out.println();
+    }
+
+    public static void guiones(int rep) {
+        for (int i = 0; i < rep; i++) {
+            System.out.print('-');    
+        }
+        System.out.println();
+    }
+
+    public static void numeros(int rep, int num) {
+        for (int i = 0; i < rep; i++) {
+            System.out.print(num);    
+        }
+        System.out.println();
+    }
+
+
+    //UNA ÚNICA FUNCIÓN
     public static void funcio(int rep, char caracter) {
         for (int i = 0; i < rep; i++) {
-            System.out.print(caracter);
-            
+            System.out.print(caracter);    
         }
         System.out.println(" ");
     }
+
 
 
 }
