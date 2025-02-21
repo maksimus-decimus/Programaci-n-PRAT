0bf9e50bc04611eaf19b19cb505650b6ed1bb1a3
maksimus-decimus <makushi2121@gmail.com>
2025-02-22 00:28:06 +0100

    3enraya

--- AEA3/tiktaktou/Tauler.java
+++ AEA3/tiktaktou/Tauler.java
@@ -5,17 +5,14 @@ public class Tauler {
 
 
 
-    
-
-
-    public Tauler(){
-        tauler = new char [3][3];
+    public Tauler(int mida){
+        tauler = new char [mida][mida];
         InicialitzarTauler();
     }
 
     public void InicialitzarTauler(){
-        for (int i = 0; i < 3; i ++){
-            for (int j = 0; j < 3; j ++){
+        for (int i = 0; i < tauler.length; i ++){
+            for (int j = 0; j < tauler[i].length; j ++){
                 tauler[i][j] = '-';
             }
         }
@@ -31,41 +28,113 @@ public class Tauler {
         System.out.println();
     }
 
-
-
-
     
+
     public boolean marcarCasella(int fila, int columna, char jugadorActual) {
         if (tauler[fila][columna] == '-') {
             tauler[fila][columna] = jugadorActual;
             return true;
         } else {
-            System.out.println("Casella ocupada, tria una altra.");
+            System.out.println("Casella ocupada, tria una altra. Passant torn per pringao");
             return false;
         }
     }
 
+
+
     public boolean haGuanyat(char jugador) {
         // Comprovar files i columnes
-        for (int i = 0; i < 3; i++) {
-            if ((tauler[i][0] == jugador && tauler[i][1] == jugador && tauler[i][2] == jugador) ||
-                (tauler[0][i] == jugador && tauler[1][i] == jugador && tauler[2][i] == jugador)) {
+        for (int i = 0; i < tauler.length; i++) {
+            boolean filaMirada = true;
+            boolean columnaMirada = true;
+
+            for (int j = 0; j < tauler.length; j++) {
+                if (tauler[i][j] != jugador) {
+                    filaMirada = false;
+                }
+
+                if (tauler[j][i] != jugador){
+                    columnaMirada = false;
+                }
+
+
+            }
+
+            if (filaMirada || columnaMirada) {
                 return true;
             }
+            
         }
 
         // Comprovar diagonals
-        if ((tauler[0][0] == jugador && tauler[1][1] == jugador && tauler[2][2] == jugador) ||
-            (tauler[0][2] == jugador && tauler[1][1] == jugador && tauler[2][0] == jugador)) {
+        boolean diagonalMirada = true;
+        boolean diagonalSecundariaMirada = true;
+
+        for (int i = 0; i < tauler.length; i++){
+            if (tauler[i][i] != jugador){
+                diagonalMirada =  false;
+            }
+
+            if (tauler[i][tauler.length - 1 - i] != jugador){
+                diagonalSecundariaMirada = false;
+            }
+
+        }
+
+        if (diagonalMirada || diagonalSecundariaMirada) {
             return true;
         }
 
         return false;
     }
 
+
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     public boolean taulerPle() {
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
+        for (int i = 0; i < tauler.length; i++) {
+            for (int j = 0; j < tauler[i].length; j++) {
                 if (tauler[i][j] == '-') {
                     return false;
                 }
