36a9cfd2b782ae27e7a20be604231b1cc0c03041
maksimus-decimus <makushi2121@gmail.com>
2025-03-29 18:11:08 +0100

    Mastermind_1

--- AEA3/MasterMind/MasterMindGame.java
+++ AEA3/MasterMind/MasterMindGame.java
@@ -1,12 +1,26 @@
 package MasterMind;
+import static MasterMind.CodeGenerator.*;
+import static MasterMind.Feedback.*;
 //Un programa per jugar al MasterMind.
 public class MasterMindGame {
-      
+    CodeGenerator codeGenerator = new CodeGenerator();
+    Feedback feedback = new Feedback();
+    Player player = new Player();
 
-    
+
+    public final static String ENCERTAT = "OOO";
+    public static String secret = "";
+    public static String resposta = "";
+
     public static void main(String[] args) {
-        Menu menu = new Menu();
-        menu.inici();
+        MasterMindGame programa = new MasterMindGame();
+
+        programa.secret = programa.codeGenerator.generarParaulaSecreta();
+        boolean encertat = false;
+        while (!encertat) {
+            resposta = programa.player.preguntarResposta();
+            encertat = programa.feedback.resoldreResposta(secret, resposta);
+        }
     }
 
     
