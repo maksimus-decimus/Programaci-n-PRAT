1ae3650f6c48c54425f7ef77ea2cd6ce14737560
Maxi Fernandez <makushi2121@gmail.com>
2025-05-05 20:02:33 +0200

    Excepcions1

---
+++ AEA4/importar/ParellsExcepcions.java
@@ -0,0 +1,21 @@
+package AEA4.importar;
+
+public class ParellsExcepcions {    
+    public static void main(String[] args) {
+
+        try {
+            //Caldrà emmagatzemar 100 enters.
+            int[] arrayParells = new int[100];
+            //Anem omplint cada posici .
+            for(int i = 0; i < arrayParells.length; i++) {
+            arrayParells[i] = 2*i;
+        }
+    
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("¡Error! Fuera de límites: " + e.getMessage());
+        }
+        
+    }
+}
+
+
